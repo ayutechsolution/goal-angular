0e12b7c209a690ff868e1866a023b2861c9c52f7
yuvraj <yuvraj.singh96@gmail.com>
2016-08-07 13:24:58 +0530

    Added About us page

--- goalnew/app.route.js
+++ goalnew/app.route.js
@@ -1,36 +1,47 @@
-(function() {
+(function () {
     'use strict';
     angular.module('goal')
-            .config(function($stateProvider, $urlRouterProvider) {
+            .config(function ($stateProvider, $urlRouterProvider) {
                 $stateProvider
                         .state('menu', {
                             url: '/menu',
                             templateUrl: "app/menu/menu.html",
                             // controller: 'homeCtrl',
-                            cache:false,
-                            abstract:true
-                            // controllerAs: 'home'
+                            cache: false,
+                            abstract: true
+                                    // controllerAs: 'home'
                         })
                         .state('menu.home', {
                             url: '/home',
-                            cache:false,
+                            cache: false,
                             views: {
-                              'menuContent': {
-                                templateUrl: "app/home/home.html",
-                                controller: 'homeCtrl',
-                                controllerAs: 'home'
-                              }
+                                'menuContent': {
+                                    templateUrl: "app/home/home.html",
+                                    controller: 'homeCtrl',
+                                    controllerAs: 'home'
+                                }
                             }
                         })
                         .state('menu.career', {
                             url: '/career',
-                            cache:false,
+                            cache: false,
                             views: {
-                              'menuContent': {
-                                templateUrl: "app/career/career.html",
-                                controller: 'careerCtrl',
-                                controllerAs: 'career'
-                              }
+                                'menuContent': {
+                                    templateUrl: "app/career/career.html",
+                                    controller: 'careerCtrl',
+                                    controllerAs: 'career'
+                                }
+                            }
+                        })
+                        .state('menu.about', {
+                            url: '/about',
+                            cache: false,
+                            views: {
+                                'menuContent': {
+                                    templateUrl: "app/about/about.html",
+                                    controller: 'aboutCtrl',
+                                    controllerAs: 'about'
+                                }
                             }
                         });
                 $urlRouterProvider.otherwise('/menu/home');
