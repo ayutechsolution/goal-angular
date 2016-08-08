ef53de3d281fe0c705651fc39007b476f9b0dea4
yuvraj <yuvraj.singh96@gmail.com>
2016-08-08 09:05:03 +0530

    Addded client Slider in home page

--- goalnew/app/home/home.controller.js
+++ goalnew/app/home/home.controller.js
@@ -1,11 +1,22 @@
-(function() {
+(function () {
     'use strict';
     angular.module('goal')
-        .controller('homeCtrl', homeCtrl);
+            .controller('homeCtrl', homeCtrl);
 
     function homeCtrl($state, $ocLazyLoad) {
         $ocLazyLoad.load('js/main.js');
         var self = this;
-        console.log('hiihi');
+
+        $(document).ready(function () {
+            $('.clientSlider').slick({
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: 2000,
+                centerMode:true,
+                pauseOnHover:false,
+            });
+        });
+
     }
 })();
